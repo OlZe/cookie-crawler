57fe51c1410f59829d471114877e9b762a5f297a
OlZe <oliverzeidler97@gmail.com>
2020-10-21 15:28:48 +0200

    will now actually crawl through the website up until a specified crawl depth

--- crawler.js
+++ crawler.js
@@ -1,7 +1,7 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const visitedUrls = new Set(); // contains all URLs that have been visited, or have been enqueued for a future visit
 const urlQueue = [];
-const maxCrawlDepth = 1;
+const maxCrawlDepth = 2;
 let domain = '';
 
 startCrawl('https://www.also.com/ec/cms5/en_6000/6000/');
@@ -11,17 +11,21 @@ startCrawl('https://www.also.com/ec/cms5/en_6000/6000/');
 
 async function startCrawl(startUrl) {
     domain = getDomainOf(startUrl);
-    console.log('Domain: ', domain);
+    console.log('Domain of start url: ', domain);
+    enqueueUrl(startUrl, 0);
+
     let driver = await new Builder().forBrowser('chrome').build();
     try {
-        enqueueUrl(startUrl);
-        let nextUrl = urlQueue.shift();
-        await visitUrl(driver, nextUrl);
-
+        while(urlQueue.length > 0) {
+            let nextUrlData = urlQueue.shift();
+            await visitUrl(driver, nextUrlData.url, nextUrlData.crawlDepth);
+        }
 
         console.log('done');
         console.log('queue: ', urlQueue);
-        console.log('URLs marked for visit:', visitedUrls);
+        console.log('visited URLs:', visitedUrls);
+        console.log('Amount of visited URLs: ', visitedUrls.size);
+
     } catch (e) {
         console.log('EXCEPTION:\n', e);
     } finally {
@@ -31,28 +35,30 @@ async function startCrawl(startUrl) {
 
 
 
-async function visitUrl(driver, url) {
+async function visitUrl(driver, url, currentCrawlDepth) {
     // Go to url
     await driver.get(url);
 
     // Get crawlable links
-    let hrefElements = await driver.findElements(By.css("a[href]"));
-    let hrefUrls = await Promise.all(hrefElements.map(e => e.getAttribute('href')));
-    let crawlableUrls = hrefUrls.filter(url => url.startsWith(domain));
+    if(currentCrawlDepth < maxCrawlDepth) {
+        let hrefElements = await driver.findElements(By.css("a[href]"));
+        let hrefUrls = await Promise.all(hrefElements.map(e => e.getAttribute('href')));
+        let crawlableUrls = hrefUrls.filter(url => url.startsWith(domain));
 
-    // enqueue crawlable links
-    crawlableUrls.forEach(enqueueUrl);
+        // enqueue crawlable links
+        crawlableUrls.forEach(url => enqueueUrl(url, currentCrawlDepth + 1));
+    }
 }
 
-function enqueueUrl(url) {
+function enqueueUrl(url, crawlDepth) {
     if(!visitedUrls.has(url)) {
-        urlQueue.push(url);
+        urlQueue.push({ url, crawlDepth });
         visitedUrls.add(url);
     }
 }
 
 function getDomainOf(url) {
-    // Get everything from http:// or https:// until the next /
+    // Get everything from 'http://' or 'https://' until the next '/'
     let domain = '';
     for(prefix of ['http://', 'https://']) {
         if(url.startsWith(prefix)) {
